878ed1749f8dd3274a26925309dea771ca1ff1f7
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2017-08-24 16:19:26 -0400

    one last attempt and then we change to mongoose

--- src/lib/assistant/google-auth.js
+++ src/lib/assistant/google-auth.js
@@ -56,16 +56,17 @@ exports.auth = (req, res) => {
   storage.codes.save(newCode, (err, saved) => {
     if (err) console.log(`Storage save error for new code: ${err}`)
     console.log(`--> saved auth code: ${util.inspect(saved)}`)
-    console.log(`--> caching redirect url: ${redir}`)
-
-    client.set(userId, redir, { expires: 600 }, (error, val) => {
-      if (err) console.log(`!!! MEM CACHE ERROR: ${error}`)
-      console.log(`--> redirect cached\n    key: ${userId}\n    val: ${val}`)
-
-      // --> send our request out to salesforce for auth
-      res.redirect(`https://assistant-prebeta.herokuapp.com/login/${userId}`)
-    })
   })
+
+  console.log(`--> caching redirect url: ${redir}`)
+
+  client.set(userId, redir, { expires: 600 }, (error, val) => {
+    if (error) console.log(`!!! MEM CACHE ERROR: ${error}`)
+    console.log(`--> redirect cached\n    key: ${userId}\n    val: ${val}`)
+  })
+
+  // --> send our request out to salesforce for auth
+  res.redirect(`https://assistant-prebeta.herokuapp.com/login/${userId}`)
 }
 
 exports.token = (req, res) => {
