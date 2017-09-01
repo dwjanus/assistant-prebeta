8b552cee83c7359e84c5176cbd835f89054f6ae0
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2017-09-01 16:31:25 -0400

    added conditional for if no articles

--- src/lib/assistant/ebu-assistant-handler.js
+++ src/lib/assistant/ebu-assistant-handler.js
@@ -43,10 +43,12 @@ const knowledge = (args, cb) => {
   const ebu = args.ebu
   const app = args.app
   const subject = app.getArgument('subject')
-  const text = 'Got some articles for you fam'
+  let text = 'I found some knowledge base articles that matched your issue. If these do not help I can submit a ticket for you'
 
   return ebu.knowledge(subject).then((articles) => {
     console.log(`--> articles retrieved:\n${util.inspect(articles)}`)
+    if (articles.length() === 0) text = 'I was unable to find any relavent articles in the knowledge base, would like me to submit a ticket?'
+    // if (surface is phone && articles.length() > 0) return processList(articles).then((list) => cb(null, list))
     return cb(null, text)
   })
   .catch(err => cb(err, null))
