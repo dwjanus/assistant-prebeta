7bea1904c5ca6409674c9d024e69626b83426a92
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2017-09-13 16:58:56 -0400

    not calling comments function despite having proper logic

--- src/lib/assistant/responses/single-record.js
+++ src/lib/assistant/responses/single-record.js
@@ -32,7 +32,7 @@ exports.single_nocontext = (args, cb) => {
       if (app.getArgument('return-type')) {
         text = `The ${app.getArgument('return-type')} is currently ${record[app.getArgument('return-type')]}`
       } else {
-        text = `${options.RecordType} ${record.CaseNumber} - ${record.Subject} / Priority: ${record.Priority} / Status: ${record.Status} ` +
+        text = `${record.SamanageESD__RecordType__c} ${record.CaseNumber} - ${record.Subject} / Priority: ${record.Priority} / Status: ${record.Status} ` +
         `Description: ${record.Description}`
       }
 
@@ -40,8 +40,7 @@ exports.single_nocontext = (args, cb) => {
         `'Description', '${record.Description}', 'CreatedDate', '${record.CreatedDate}', 'CaseNumber', '${record.CaseNumber}', ` +
         `'SamanageESD__OwnerName__c', '${record.SamanageESD__OwnerName__c}', 'SamanageESD__Assignee_Name__c', '${record.SamanageESD__Assignee_Name__c}', ` +
         `'Priority', '${record.Priority}', 'Status', '${record.Status}', 'SamanageESD__hasComments__c', '${record.SamanageESD__hasComments__c}', ` +
-        `'HasCommentsUnreadByOwner', '${record.HasCommentsUnreadByOwner}', 'Last_Comment__c', '${record.Last_Comment__c}', ` +
-        `'RecordTypeId', '${record.RecordTypeId}')`
+        `'SamanageESD__RecordType__c', '${record.SamanageESD__RecordType__c}', 'RecordTypeId', '${record.RecordTypeId}')`
 
       return query(saveRecordStr).then(() => {
         if (app.getArgument('yesno') && record) text = `Yes, ${text}`
@@ -68,29 +67,31 @@ exports.single_details = (args, cb) => {
   let text = `The ${returnType} is currently ${latestRecord[returnType]}`
   console.log(`--> record after jsonify:\n${util.inspect(latestRecord)}`)
 
-  if (returnType !== 'Latest Comment' || 'Comments') return cb(null, text)
-  return ebu.comments(latestRecord.Id, user.sf_id).then((comments) => {
-    console.log('--> just got comments back')
-    if (comments) {
-      console.log('---> they are not empty')
-      if (returnType === 'Latest Comment') {
-        const latest = comments[0]
-        // add parse for date so text version says Aug. 29 at {time} and speech takes normal date-time field from object
-        text = `The most recent comment is "${latest.Body}" and was posted by ${latest.User} on ${latest.CreatedDate}. `
-        if (latest.CommentCount === 0) text += 'There are no responses to this. Would you like to post a reply?'
-        if (latest.CommentCount === 1) text += 'There is 1 response, would you like to view it?'
-        else text += `There are ${latest.CommentCount} responses, would you like to view them?`
-      } else {
-        for (const c of comments) {
-          text += `${c.CreatedDate}: "${c.Body}" posted by ${c.User}`
+  if (returnType === 'Latest Comment') {
+    console.log(`--> return type is: ${returnType}`)
+    return ebu.comments(latestRecord.Id, user.sf_id).then((comments) => {
+      console.log('--> just got comments back')
+      if (comments) {
+        console.log('---> they are not empty')
+        if (returnType === 'Latest Comment') {
+          const latest = comments[0]
+          // add parse for date so text version says Aug. 29 at {time} and speech takes normal date-time field from object
+          text = `The most recent comment is "${latest.Body}" and was posted by ${latest.User} on ${latest.CreatedDate}. `
+          if (latest.CommentCount === 0) text += 'There are no responses to this. Would you like to post a reply?'
+          if (latest.CommentCount === 1) text += 'There is 1 response, would you like to view it?'
+          else text += `There are ${latest.CommentCount} responses, would you like to view them?`
+        } else {
+          for (const c of comments) {
+            text += `${c.CreatedDate}: "${c.Body}" posted by ${c.User}`
+          }
         }
+      } else {
+        text = 'There are no public comments, would you like to post one?'
       }
-    } else {
-      text = 'There are no public comments, would you like to post one?'
-    }
-
-    return cb(null, text)
-  })
+      return cb(null, text)
+    })
+  }
+  return cb(null, text)
 }
 
 exports.single_change = (args, cb) => {
