597e364a4374136bee13bac4591c421bd7c5182a
chrissamanage <chris.walls@samanage.com>
2017-09-14 13:46:36 -0400

    Updating auths

--- src/lib/samanage/ebu-api.js
+++ src/lib/samanage/ebu-api.js
@@ -69,7 +69,7 @@ export default ((userId) => {
     .then((user) => {
       if (!user.sf_id) {
         console.log('--! no connection object found !---\n    returning link now ')
-        return reject({ text: `✋ Hold your horses!\nVisit this URL to login to Salesforce: https://assistant-prebeta.herokuapp.com/login/${userId}` })
+        return reject({ text: `✋ Hold your horses!\nVisit this URL to login to Salesforce: https://assistant-prebeta-walls.herokuapp.com/login/${userId}` })
       }
 
       let conn = new jsforce.Connection({
@@ -111,7 +111,7 @@ export default ((userId) => {
           })
           .catch((referr) => {
             console.log(`!!! refresh event error! ${referr}`)
-            return reject({ text: `✋ Whoa now! You need to reauthorize first.\nVisit this URL to login to Salesforce: https://assistant-prebeta.herokuapp.com/login/${userId}` })
+            return reject({ text: `✋ Whoa now! You need to reauthorize first.\nVisit this URL to login to Salesforce: https://assistant-prebeta-walls.herokuapp.com/login/${userId}` })
           })
         }
 
