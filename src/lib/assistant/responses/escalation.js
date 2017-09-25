dcea571e11fc50429a48fcd32690486cadd7293b
chrissamanage <chris.walls@samanage.com>
2017-09-25 13:33:19 -0400

    Updating escalation

--- src/lib/assistant/responses/escalation.js
+++ src/lib/assistant/responses/escalation.js
@@ -5,19 +5,19 @@ import util from 'util'
 const query = db.querySql
 
 exports.agent = (args, cb) => {
-  console.log(`\n--> Inside escalate.agent`)
+  console.log(`\n--> Inside escalate`)
   console.log(`\n--> ${util.inspect(args)}`)
   const app = args.app
   const ebu = args.ebu
   const user = args.user
-
-}
-
-exports.requester = (args, cb) => {
-  console.log(`\n--> Inside escalate.requester`)
-  console.log(`\n--> ${util.inspect(args)}`)
-  const app = args.app
-  const ebu = args.ebu
-  const user = args.user
-
+  let text = ''
+  let options = {
+    CaseNumber: app.getArgument('CaseNumber'),
+    RecordType: app.getArgument('record-type')
+  }
+  if (!app.getArgument('record-type')) options.RecordType = 'Incident'
+  console.log(`options: ${util.inspect(options)}`)
+  return ebu.singleRecord(options).then((case) => {
+    console.log(`\n--> Case: ${util.inspect(case)}`);
+  })
 }
