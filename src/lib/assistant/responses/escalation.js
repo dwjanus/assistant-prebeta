d7e2add662d60558c7e177491fba07ce38e51d3f
chrissamanage <chris.walls@samanage.com>
2017-09-26 14:31:30 -0400

    Reverting to real logic

--- src/lib/assistant/responses/escalation.js
+++ src/lib/assistant/responses/escalation.js
@@ -34,32 +34,15 @@ exports.escalation = (args, cb) => {
   console.log(`options: ${util.inspect(options)}`)
   console.log(`\n--> escalation_options: ${util.inspect(escalation_options)}`)
 
-  // return ebu.singleRecord(options).then((record) => {
-  //   console.log(`\n--> record returned from ebu api`)
-  //   if (record) {
-  //     console.log(`Escalation records: ${util.inspect(record)}`)
-  //     text += 'Found a record'
-  //   }
-  //   else {
-  //     text += `I could not find ${options.RecordType} number ${options.CaseNumber}`
-  //   }
-  //   return cb(null, text)
-  // })
-  return ebu.metrics(options).then((records) => {
-    console.log(`\n--> records returned from ebu api`)
-    let startClosedDate = dateFormat(options.DatePeriod.split('/')[0],'fullDate')
-    let endClosedDate = dateFormat(options.DatePeriod.split('/')[1],'fullDate')
-    if (!_.isEmpty(records)) {
-      if (records.length > 1) {
-        text = `${records.length} ${options.RecordType}s were ${options.StatusChange} between ${startClosedDate} and ${endClosedDate} `
-        if (app.getArgument('yesno')) text = `Yes, ${text}`
-        text += `The most recently active being ${options.RecordType} ` +
-       `${parseInt(records[0].CaseNumber, 10)}: ${records[0].Subject}`
-      } else {
-        text = `I found ${options.RecordType} ${records[0].CaseNumber}: ${records[0].Subject}`
-      }
-    } else {
-      text = `No ${options.RecordType}s were ${options.StatusChange} between ${startClosedDate} and ${endClosedDate}`
+  return ebu.singleRecord(options).then((record) => {
+    console.log(`\n--> record returned from ebu api`)
+    if (record) {
+      console.log(`Escalation records: ${util.inspect(record)}`)
+      text += `Found a ${options.RecordType} # ${record.CaseNumber}\n`
+      text += `Let's collect a little more information`
+    }
+    else {
+      text += `I could not find ${options.RecordType} number ${options.CaseNumber}`
     }
     return cb(null, text)
   })
