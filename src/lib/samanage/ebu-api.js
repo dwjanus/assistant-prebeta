f80be250165665f61c77fca843a4829af344f831
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2017-09-27 15:31:54 -0400

    im dumb

--- src/lib/samanage/ebu-api.js
+++ src/lib/samanage/ebu-api.js
@@ -14,7 +14,7 @@ const formatCaseNumber = (number) => {
 }
 
 const addslashes = (str) => {
-  return (`${str}`).replace(/[&:"'-*+~?^${}()|[\]\\]/g, '\\$&').replace(/\u0000/g, '\\0')
+  str.replace(/[&!:"'-*+~?^${}()|[\]\\]/g, '\\$&').replace(/\u0000/g, '\\0')
 }
 
 const recordType = {
