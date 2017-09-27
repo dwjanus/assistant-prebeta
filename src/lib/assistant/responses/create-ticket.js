48f7c06f2818a56a1c5203169b59aabacf281801
Devin Janus <devinjanus@D.Janus-MBP-8GB>
2017-09-27 18:09:56 -0400

    everything around articles works, just trying to persist subject data

--- src/lib/assistant/responses/create-ticket.js
+++ src/lib/assistant/responses/create-ticket.js
@@ -9,13 +9,12 @@ exports.createTicket_knowledge = (args, cb) => {
 
   const app = args.app
   const subject = app.getContextArgument('KNOWLEDGE_NO_CONTEXT', 'Subject')
-  const subject2 = app.parms.Subject
-  const subject3 = app.body.originalRequest
+  const subject2 = app.body.originalRequest
   const text = `Sure thing! So far I have ${subject} as the subject for your incident. If you ` +
     'would like to change the subject, add a description, set the priority, or anything else, ' +
     'simply tell me what field values you would like. Or I can submit with defaults.'
 
-  console.log(`--> subject: ${subject}\n--> subject2: ${subject2}\n--> subject3: ${util.inspect(subject3)}`)
+  console.log(`--> subject: ${subject}\n--> subject2: ${util.inspect(subject2)}`)
   return cb(null, text)
 }
 
