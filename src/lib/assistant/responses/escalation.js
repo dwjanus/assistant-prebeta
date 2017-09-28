f01533346fa9c6d0878c07d80e4330dd229d8479
chrissamanage <chris.walls@samanage.com>
2017-09-28 14:34:17 -0400

    Printing escalationOptions

--- src/lib/assistant/responses/escalation.js
+++ src/lib/assistant/responses/escalation.js
@@ -47,6 +47,7 @@ exports.escalation = (args, cb) => {
         SamanageESD__Escalated_Date__c: dateFormat(now, 'isoDateTime'),
         SamanageESD__Escalated_By__c: user.sf_id,
       }
+      console.log(`Escalation Options: ${util.inspect(escalationOptions)}`)
       return ebu.update(escalationOptions).then((result) => {
         text += `Escalation complete` // edit later
         return cb(null, text)
